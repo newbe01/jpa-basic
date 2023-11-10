3c828c03ed1f0cbbf846f100316afddb510b6f4b
newbe01 <pkwaa3@gmail.com>
2023-11-10 21:09:07 +0900

    jpa -basic

--- src/main/java/jpabasic/basic/JpaMain.java
+++ src/main/java/jpabasic/basic/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,15 +15,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 1L);
-            member.setName("third");
+//            Member member = em.find(Member.class, 1L);
+//            member.setName("third");
 
             //saveQuery
 //            em.persist(member);
 
             //delete Query
 //            em.remove(member);
+
+            //jpql
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(2)     paging
+                    .getResultList();
             
+            for(Member m : result) {
+                System.out.println("m.getName() = " + m.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
