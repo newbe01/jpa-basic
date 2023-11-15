ec322d0f4d88d311b58e9cd81a4898bcbed8a216
newbe01 <pkwaa3@gmail.com>
2023-11-15 21:59:43 +0900

    joinTable 전략
    기본설정시 단일테이블전략으로 하나의 통합테이블로만든다
    `@Inheritance()`로 속성을 설정해주면 JoinTable전략이 사용가능하다

---
+++ src/main/java/jpabasic/basic/ItemMain.java
@@ -0,0 +1,41 @@
+package jpabasic.basic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class ItemMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA2");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Movie movie = new Movie();
+            movie.setDirector("aa");
+            movie.setActor("bb");
+            movie.setName("name");
+            movie.setPrice(10000);
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
