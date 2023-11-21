b0e34fab684f69649f1815b2df7aea65d8fc7ace
newbe01 <pkwaa3@gmail.com>
2023-11-21 10:50:38 +0900

    fetch join1

--- JPQL/src/main/java/com/example/jpql/ex/JpqlMain2.java
+++ JPQL/src/main/java/com/example/jpql/ex/JpqlMain2.java
@@ -17,16 +17,34 @@ public class JpqlMain2 {
         try {
 
             Team team = new Team();
-            team.setName("team");
+            team.setName("teamA");
             em.persist(team);
 
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
             Member member = new Member();
-            member.setUsername("name");
+            member.setUsername("name1");
             member.setAge(14);
             member.setMemberType(MemberType.ADMIN);
             member.setTeam(team);
 
+            Member member2 = new Member();
+            member2.setUsername("name2");
+            member2.setAge(15);
+            member2.setMemberType(MemberType.ADMIN);
+            member2.setTeam(team);
+
+            Member member3 = new Member();
+            member3.setUsername("name3");
+            member3.setAge(16);
+            member3.setMemberType(MemberType.ADMIN);
+            member3.setTeam(team2);
+
             em.persist(member);
+            em.persist(member2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
@@ -35,9 +53,23 @@ public class JpqlMain2 {
 //            String sql = "select m.username from Member m";     //상태필드, 더이상 탐색 불가능
 //            String sql = "select m.team from Member m";        // 단일값 연관경로, 묵시적 내부조인 발생 추가 탐색가능 team.name~
 //            String sql = "select t.members.size from Team t";        // 컬렉션 값 연관경로, 묵시적 내부조인 발생, 추가 탐색 X
-            String sql = "select m from Team t join t.members m";      // 명시적 조인, 추가 탐색가능
-            em.createQuery(sql).getResultList();
+//            String sql = "select m from Team t join t.members m";      // 명시적 조인, 추가 탐색가능
+//            em.createQuery(sql).getResultList();
 
+//  fetch join
+//  연관된 엔티티도 함께조회(즉시로딩) 객체그래프를 한번에 조회하는개념
+//            String sql = "select m from Member m join fetch m.team";
+            String sql = "select distinct t from Team t join fetch t.members";  // 컬렉션의 중복도 함께 제거
+            List<Team> resultList = em.createQuery(sql, Team.class).getResultList();
+
+            for (Team findTeam : resultList) {
+                System.out.println("member = " + findTeam.getName() +"  ,"+ findTeam.getMembers().size());
+
+                for (Member m : findTeam.getMembers()) {
+                    System.out.println("m = " + m);
+                }
+                
+            }
 
             tx.commit();
         } catch (Exception e) {
